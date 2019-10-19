f359d7920a8eb263e452d2693682fa82fe593a01
AlvaroMarques <sc.poi458@gmail.com>
2019-10-19 19:44:36 -0300

    mandei o server.py, mano eu nao sei escrever socorro

---
+++ serverpy.py
@@ -0,0 +1,68 @@
+import argparse
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"{message}"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
+    def do_GET(self):
+        dictget = parse_qs(self.path[2:])
+        pi = int(dictget.get("pi",["-1"])[0])
+        pf = int(dictget.get("pf",["-1"])[0])
+        #response = str(makemove)
+        r = {"pi":int(pi), "pf":int(pf)}
+        response =  "" 
+        for k in r.keys():
+            response += str(k)+" "+str(r[k])+"\n"
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/json")
+        self.send_header("Content-length", str(len(str(response))))
+        self.end_headers()
+        self.wfile.write(self._html(str(response)))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    #Não vamos precisar de post nesse rolê
+    def do_POST(self):
+        self._set_headers()
+        self.wfile.write(self._html("POST!"))
+
+
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
